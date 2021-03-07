4b96f448538e9275795571995a135ff952744282
Velnbur <kirillbaybula@gmail.com>
2021-03-07 10:03:55 +0000

    changed install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
-echo "Moving scripts..."
-mv ./tablet-mode/tablet-mode.sh -t /usr/local/bin/
-mv ./laptop-mode/laptop-mode.sh -t /usr/local/bin/
-chmod +x /usr/local/bin/laptop-mode.sh
-chmod +x /usr/local/bin/tablet-mode.sh
+LIST_OF_APPS="tmux vim ssh git htop zsh"
+sudo apt update
+sudo apt install "$LIST_OF_APPS"
 
-echo "Creating apps"
-mv ./tablet-mode/tablet-mode.desktop -t /usr/share/applications/
-mv ./laptop-mode/laptop-mode.desktop -t /usr/share/applications/
+echo "Moving config files..."
+cp ./.tmux.conf ./.profile ./.vimrc ./.zshrc -t $HOME
 
-echo "Moving Icons"
-mkdir /usr/share/icons/mods
-mv ./icons/* -t /usr/icons/mods/
+if [[ -d "$HOME/.doom.d" ]] ; then
+    echo "Moving init.el to .doom.d"
+    cp ./init.el -t $HOME/.doom.d
+fi
+
+read -p "Do you want to instaal tablet/laptop-modes? [y/n]" answer
+
+if [[ "$asnwer" == "y" ]] ; then
+    echo "Moving scripts..."
+    sudo cp ./tablet-mode/tablet-mode.sh -t /usr/local/bin/
+    sudo cp ./laptop-mode/laptop-mode.sh -t /usr/local/bin/
+    sudo chmod +x /usr/local/bin/laptop-mode.sh
+    sudo chmod +x /usr/local/bin/tablet-mode.sh
+
+    echo "Creating apps"
+    sudo cp ./tablet-mode/tablet-mode.desktop -t /usr/share/applications/
+    sudo cp ./laptop-mode/laptop-mode.desktop -t /usr/share/applications/
+
+    echo "Moving Icons"
+    sudo mkdir /usr/share/icons/mods
+    sudo cp ./icons/* -t /usr/icons/mods/
+fi
 
 echo "Done!"
-echo "Please log out and then log in"
+
+if [[ "$answer" == "y" ]] ; then
+    echo "Please log out and then log in"
+fi
